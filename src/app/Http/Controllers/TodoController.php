37d6e7af9a62e977cc72406bf140524bef1b757d
yurin <hoshinana.eight.nine.ten.y@gmail.com>
2026-01-08 23:20:15 +0900

    2-3テーブルの作成

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+    return view('index');
+    }
+}
